50739831378f0a7857a8885e110bcae34b7d77a3
LeeBamberTGC <LeeBamberTGC@0d003e0e-0b79-11df-bf12-379292acd595>
2010-03-31 20:14:59 +0000

    added BAT to ease the process of compiling fresh DLLs and copying them directly to the UPDATE folder (under Lees directory structure of C:TGSSHARED and inside this folder the public and private shared folders Dark Basic Pro Shared (which is private) and Dark Basic Public Shared (which is public).

---
+++ trunk/ darkbasicpro --username LeeBamberTGC/copy_DLLS_to_UPDATE.bat
@@ -0,0 +1,6 @@
+echo "Copying all work DLLs to update folder..."
+echo ""
+cd "Dark Basic Pro\Compiler\plugins"
+copy "*.dll" ".\..\..\..\..\Dark Basic Pro Shared\UPDATES\U7.5\Main\Compiler\plugins\"
+echo "finished."
+cmd.exe
