024b007732878e7d7ee707b05f51770c884c2d75
IanM.Matrix1 <IanM.Matrix1@0d003e0e-0b79-11df-bf12-379292acd595>
2010-10-26 19:46:13 +0000

    Replace CData class with std::map Replaced dual containers with a single container and a class structure Pretty much rewrote every command.

--- trunk/ darkbasicpro --username LeeBamberTGC/Dark Basic Pro SDK/Shared/Vectors/cVectorManagerC.cpp
+++ trunk/ darkbasicpro --username LeeBamberTGC/Dark Basic Pro SDK/Shared/Vectors/cVectorManagerC.cpp
@@ -3,86 +3,53 @@
 
 cDataManager::cDataManager ( )
 {
-	
 }
 
 cDataManager::~cDataManager ( )
 {
-	int iTemp;
-
-	link* pCheck = m_List.m_start;
-
-	for ( iTemp = 0; iTemp < m_List.m_count; iTemp++ )
-	{
-		void* ptr = NULL;
-		ptr = this->GetData ( pCheck->id );
-
-		if ( ptr == NULL )
-			continue;
-
-		delete ptr;
-
-		pCheck = pCheck->next;
-	}
+    ClearAll();
 }
 
-bool cDataManager::Add ( void* pData, int iID )
+void cDataManager::ClearAll ( )
 {
-	///////////////////////////////////////
-	// check if an object already exists //
-	// with the same id, if it does then //
-	// delete it                         //
-	///////////////////////////////////////
-	void* ptr = NULL;
-	ptr = ( void* ) m_List.Get ( iID );
-			
-	if ( ptr != NULL )
-		m_List.Delete ( iID );
-	///////////////////////////////////////
+    for (ListPtr pCheck = m_List.begin(); pCheck != m_List.end(); ++pCheck)
+    {
+        // Release the texture and texture name
+        delete pCheck->second;
+    }
 
-	///////////////////////////////////////
-	// create a new object and insert in //
-	// the list                          //
-	///////////////////////////////////////
-	//void* test;
-	//test = new void*;
+    // Now clear the list
+    m_List.clear();
+}
 
-	//memset ( test,     0, sizeof ( test  ) );
-	//memcpy ( test, pData, sizeof ( sData ) );
+bool cDataManager::Add ( BaseVector* pData, int iID )
+{
+    // Attempt to insert
+    ListInsertStatus s = m_List.insert( std::make_pair( iID, pData ) );
 
-	//m_List.Add ( iID, ( VOID* ) test, 0, 1 );
-	m_List.Add ( iID, ( VOID* ) pData, 0, 1 );
-	///////////////////////////////////////
+    // If insert fails due to already existing, replace instead
+    if (!s.second)
+    {
+        delete s.first->second;
+        s.first->second = pData;
+    }
 
 	return true;
 }
 
 bool cDataManager::Delete ( int iID )
 {
-	void* ptr = NULL;
-	ptr = ( void* ) m_List.Get ( iID );
-	if(ptr) delete ptr;
-	m_List.Delete ( iID );
+    ListPtr p = m_List.find( iID );
+    if (p != m_List.end())
+    {
+        delete p->second;
+        m_List.erase( p );
+    }
 	return true;
 }
 
-void* cDataManager::GetData ( int iID )
-{		
-	return ( void* ) m_List.Get ( iID );
-}
-
-void cDataManager::Update ( void )
+BaseVector* cDataManager::GetData ( int iID )
 {
-	link* check = m_List.m_start;
-
-	for ( int temp = 0; temp < m_List.m_count; temp++ )
-	{
-		void* ptr = NULL;
-		ptr = this->GetData ( check->id );
-
-		if ( ptr == NULL )
-			return;
-
-		check = check->next;
-	}
+    ListPtr p = m_List.find( iID );
+    return (p == m_List.end()) ? NULL : p->second;
 }
--- trunk/ darkbasicpro --username LeeBamberTGC/Dark Basic Pro SDK/Shared/Vectors/cVectorManagerC.h
+++ trunk/ darkbasicpro --username LeeBamberTGC/Dark Basic Pro SDK/Shared/Vectors/cVectorManagerC.h
@@ -1,7 +1,6 @@
 #ifndef _CVECTORMANAGER_H_
 #define _CVECTORMANAGER_H_
 
-#include ".\..\data\cdatac.h"
 #include "cVectorDataC.h"
 
 #pragma comment ( lib, "dxguid.lib" )
@@ -23,21 +22,79 @@
 #include <windows.h>   
 #include <windowsx.h> 
 
+#include <map>
+
+#define TYPE_VECTOR2	1
+#define TYPE_VECTOR3	2
+#define TYPE_VECTOR4	3
+#define TYPE_MATRIX		4
+
+class BaseVector
+{
+private:
+    int Type;
+public:
+    BaseVector(int Type_) : Type( Type_ ) {}
+    int GetType() const { return Type; }
+};
+
+class Vector2 : public BaseVector
+{
+private:
+    D3DXVECTOR2 V;
+public:
+    Vector2() : BaseVector(TYPE_VECTOR2), V(0.0f, 0.0f) { }
+    D3DXVECTOR2& Get() { return V; }
+};
+
+class Vector3 : public BaseVector
+{
+private:
+    D3DXVECTOR3 V;
+public:
+    Vector3() : BaseVector(TYPE_VECTOR3), V(0.0f, 0.0f, 0.0f) { }
+    D3DXVECTOR3& Get() { return V; }
+};
+
+class Vector4 : public BaseVector
+{
+private:
+    D3DXVECTOR4 V;
+public:
+    Vector4() : BaseVector(TYPE_VECTOR4), V(0.0f, 0.0f, 0.0f, 0.0f) { }
+    D3DXVECTOR4& Get() { return V; }
+};
+
+class Matrix : public BaseVector
+{
+private:
+    D3DXMATRIX V;
+public:
+    Matrix() : BaseVector(TYPE_MATRIX), V()
+    {
+        memset( &V, 0, sizeof(V) );
+    }
+    D3DXMATRIX& Get() { return V; }
+};
+
 class cDataManager
 {
 	private:
-		CData	m_List;
+        typedef std::map<int, BaseVector*>	List_t;
+        typedef List_t::iterator			ListPtr;
+        typedef std::pair<ListPtr, bool>    ListInsertStatus;
+
+        List_t	m_List;
 
 	public:
-		cDataManager  ( );
-		~cDataManager ( );
+		cDataManager  		( );
+		~cDataManager 		( );
+		void ClearAll		( );
 
-		bool Add    ( void* pData, int iID );
-		bool Delete ( int iID );
+		bool Add    		( BaseVector* pData, int iID );
+		bool Delete 		( int iID );
 
-		void* GetData ( int iID );
-
-		void Update ( void );
+		BaseVector* GetData ( int iID );
 };
 
 #endif _CVECTORMANAGER_H_
