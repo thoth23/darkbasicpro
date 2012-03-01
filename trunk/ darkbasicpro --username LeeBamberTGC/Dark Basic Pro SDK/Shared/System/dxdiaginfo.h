601f74a6de3c0b6be49adcdf708b16259746520f
IanM.Matrix1 <IanM.Matrix1@0d003e0e-0b79-11df-bf12-379292acd595>
2012-03-01 14:18:54 +0000

    SYSTEM DMEM AVAILABLE function crash. Rather than attempt to trace the many crashes in the dxdiaginfo class, elected instead to cut down the code to the minimum necessary to support querying of the amount of video memory available. An extra benefit is that the function should also be faster than it used to be (when it worked, at least).

--- trunk/ darkbasicpro --username LeeBamberTGC/Dark Basic Pro SDK/Shared/System/dxdiaginfo.h
+++ trunk/ darkbasicpro --username LeeBamberTGC/Dark Basic Pro SDK/Shared/System/dxdiaginfo.h
@@ -24,6 +24,10 @@
 //-----------------------------------------------------------------------------
 // Defines, and constants
 //-----------------------------------------------------------------------------
+
+// IRM 20120301 - Problems throughout the class - Removed everything that was not used
+//                to ease debug.
+
 class CDxDiagInfo
 {
 public:
@@ -34,56 +38,16 @@ public:
     HRESULT QueryDxDiagViaDll();
 
 public:
-    HRESULT GetSystemInfo( SysInfo** ppSysInfo );
-    HRESULT GetSystemDevices( vector<SystemDevice*>& vSystemDevices );
-    HRESULT GatherSystemDeviceDriverList( IDxDiagContainer* pParent, vector<FileNode*>& vDriverList );
-    HRESULT GatherFileNodeInst( FileNode* pFileNode, IDxDiagContainer* pObject );
-    HRESULT GetDirectXFilesInfo( FileInfo** ppFileInfo );
     HRESULT GetDisplayInfo( vector<DisplayInfo*>& vDisplayInfo );
-    HRESULT GatherDXVA_DeinterlaceCaps( IDxDiagContainer* pParent, vector<DxDiag_DXVA_DeinterlaceCaps*>& vDXVACaps );
-    HRESULT GetSoundInfo( vector<SoundInfo*>& vSoundInfos, vector<SoundCaptureInfo*>& vSoundCaptureInfos );
-    HRESULT GetMusicInfo( MusicInfo** ppMusicInfo );
-    HRESULT GetInputInfo( InputInfo** ppInputInfo );
-    HRESULT GatherInputRelatedDeviceInst( InputRelatedDeviceInfo* pInputRelatedDevice, IDxDiagContainer* pContainer );
-    HRESULT GatherInputRelatedDeviceInstDrivers( InputRelatedDeviceInfo* pInputRelatedDevice, IDxDiagContainer* pChild );
-    HRESULT GetNetworkInfo( NetInfo** ppNetInfo );
-    HRESULT GetShowInfo( ShowInfo** ppShowInfo );
-    HRESULT GetLogicalDiskInfo( vector<LogicalDisk*>& vLogicalDisks );
-    
     HRESULT GetStringValue( IDxDiagContainer* pObject, WCHAR* wstrName, TCHAR* strValue, int nStrLen );
-    HRESULT GetUIntValue( IDxDiagContainer* pObject, WCHAR* wstrName, DWORD* pdwValue );
-    HRESULT GetIntValue( IDxDiagContainer* pObject, WCHAR* wstrName, LONG* pnValue );
-    HRESULT GetBoolValue( IDxDiagContainer* pObject, WCHAR* wstrName, BOOL* pbValue );
-    HRESULT GetInt64Value( IDxDiagContainer* pObject, WCHAR* wstrName, ULONGLONG* pullValue );
-
-    VOID DestroyFileList(FileInfo* pFileInfo);
-    VOID DestroySystemDevice( vector<SystemDevice*>& vSystemDevices );
     VOID DestroyDisplayInfo( vector<DisplayInfo*>& vDisplayInfo );
-    VOID DeleteInputTree( vector<InputRelatedDeviceInfo*>& vDeviceList );
-    VOID DeleteFileList( vector<FileNode*>& vDriverList );
-    VOID DestroyInputInfo(InputInfo* pInputInfo);
-    VOID DestroyMusicInfo(MusicInfo* pMusicInfo);
-    VOID DestroyNetworkInfo(NetInfo* pNetInfo);
-    VOID DestroySoundInfo(vector<SoundInfo*>& vSoundInfos);
-    VOID DestroySoundCaptureInfo(vector<SoundCaptureInfo*>& vSoundCaptureInfos);
-    VOID DestroyShowInfo(ShowInfo* pShowInfo);
 
     IDxDiagProvider*  m_pDxDiagProvider;
     IDxDiagContainer* m_pDxDiagRoot;
     BOOL              m_bCleanupCOM;
 
 public:
-    SysInfo*                    m_pSysInfo;
-    vector<SystemDevice*>       m_vSystemDevices;
-    FileInfo*                   m_pFileInfo;
     vector<DisplayInfo*>        m_vDisplayInfo;
-    vector<SoundInfo*>          m_vSoundInfos;
-    vector<SoundCaptureInfo*>   m_vSoundCaptureInfos;
-    MusicInfo*                  m_pMusicInfo;
-    InputInfo*                  m_pInputInfo;
-    NetInfo*                    m_pNetInfo;
-    ShowInfo*                   m_pShowInfo;
-    vector<LogicalDisk*>        m_vLogicalDiskList;
 };
 
 
